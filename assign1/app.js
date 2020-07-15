7c0ca35a600105891f953b038c53641b6f9a124c
sara chafai <sara.chafai@atos.net>
2020-07-15 16:53:22 +0200

    add files

---
+++ assign1/app.js
@@ -0,0 +1,26 @@
+(function() {
+    "use strict";
+    
+    angular.module("LunchCheck",[])
+    .controller("LunchCheckController", LunchCheckController);
+    
+    LunchCheckController.$inject = ["$scope"]
+    
+    function LunchCheckController ($scope) {
+        $scope.message = "";
+        $scope.menu = "" ;
+        $scope.checkLunch = function () {
+            var menu_array = $scope.menu.split(',');
+        
+            if ($scope.menu == "") {
+                $scope.message = "Please enter your lunch list"
+            } else if (menu_array.length <=3 ) {
+                $scope.message = "Enjoy!" ;
+            } else {
+                $scope.message = "Too much!";
+            }
+        }
+    }	
+    
+    })();
+    
